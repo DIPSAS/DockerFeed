ba24714824f78263106906bc842b3c61cfc5f3bf
Hans Erik Heggem <heh@dips.no>
2019-10-04 15:22:54 +0200

    updated docker build system

--- tests/TestInfrastructureHandler.py
+++ tests/TestInfrastructureHandler.py
@@ -1,11 +1,13 @@
 import unittest
 from DockerFeed import InfrastructureHandler
 from tests import TestUtilities
+from DockerBuildSystem import DockerSwarmTools
 
 class TestInfrastructureHandler(unittest.TestCase):
 
     def test_CreateRemoveInfrastructure(self):
         infrastructureFile = "tests/testStacks/docker-compose.infrastructure.yml"
+        DockerSwarmTools.StartSwarm()
         InfrastructureHandler.CreateInfrastructure(infrastructureFile)
         TestUtilities.AssertInfrastructureExists(True)
         InfrastructureHandler.RemoveInfrastructure(infrastructureFile)
